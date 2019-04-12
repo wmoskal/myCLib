2c8959b8752bcac0c22e12a708de8f07d5fd0fb7
socs <socs@socsvm>
2019-04-12 13:17:31 -0400

    Function Prototypes for base Functions

---
+++ myclib.h
@@ -0,0 +1,55 @@
+#ifndef MYCLIB_H
+#define MYCLIB_H
+
+typedef struct intNode
+{
+  int key;
+  struct intNode *left;
+  struct intNode *right;
+}intNode;
+
+typedef struct floatNode
+{
+  float key;
+  struct floatNode *left;
+  struct floatNode *right;
+}floatNode;
+
+typedef struct stringNode
+{
+  char *key;
+  struct stringNode *left;
+  struct stringNode *right;
+}stringNode;
+
+int *heapSortInt(int *arr);
+intNode *minIntHeapConstruct(int *arr);
+intNode *maxIntHeapConstruct(int *arr);
+float *heapSortFloat(float *arr);
+floatNode *minFloatHeapConstruct(float *arr);
+floatNode *maxFloatHeapConstruct(float *arr);
+char **heapSortString(char **);
+stringNode *minStringHeapConstruct(char **arr);
+stringNode *maxStringHeapConstruct(char **arr);
+
+int intBinarySearch(int *arrSorted);
+float floatBinarySearch(float *arrSorted);
+char *stringBinarySearch(char **arrSorted);
+
+intNode *binaryIntSearchTreeConstruct(int *arr);
+floatNode *binaryFloatSearchTreeConstruct(float *arr);
+stringNode *binaryStringSearchTreeConstruct(char **arr);
+
+void insertIntIntoBST(intNode *root, int toInsert);
+int deleteIntFromBST(intNode *root, int toDelete);
+void printIntBST(intNode *root);
+
+void insertFloatIntoBST(floatNode *root, float toInsert);
+float deleteFloatFromBST(floatNode *root, float toDelete);
+void printFloatBST(floatNode *root);
+
+void insertStringIntoBST(stringNode *root, char *toInsert);
+float deleteFloatFromBST(stringNode *root, char *toDelete);
+void printFloatBST(stringNode *root);
+
+#endif
